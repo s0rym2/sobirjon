c943272e3538630596c556f0b0b9b79eba1a2bfb
s0rym2 <sobirjonturayov7@gmail.com>
2022-06-25 11:07:49 +0500

    new some file

---
+++ app.js
@@ -0,0 +1,8 @@
+let fruits = ['apple','orenge','stawabery','banan','chery','limon','kiwi']
+
+// fruits.push('watermelon')
+// fruits.unshift('ananas')
+
+for(let i=0; i <fruits.length; i++)
+
+console.log("№:" + i + fruits [i]); 
